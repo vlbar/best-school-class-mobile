76b42073fb85cb521833d3916ec6727c967f1a43
Vladislav Baranov <62684874+vlbar@users.noreply.github.com>
2021-10-11 19:36:14 +0300

    Init react native project

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const App = () => {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Best School Class Mobile</Text>
+    </View>
+  );
+};
+
+export default App;
