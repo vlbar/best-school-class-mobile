2a197e868d90963b334cc11f70ef719d30039be1
Vladislav Baranov <62684874+vlbar@users.noreply.github.com>
2021-11-13 17:42:56 +0300

    Fixed internationalization initialization before getting user language

--- App.js
+++ App.js
@@ -1,18 +1,21 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { View } from 'react-native';
+
 import StartNavigation from './src/components/navigation/StartNavigation';
 import MainNavigation from './src/components/navigation/MainNavigation';
 import { configureInternationalization } from './src/utils/Internationalization';
 
-configureInternationalization();
-
 const App = () => {
   const [isSignedIn, setIsSignedIn] = useState(false);
+  const [isInitialized, setIsInitialized] = useState(false);
 
-  return !isSignedIn ? (
-    <StartNavigation onLoginSuccess={() => setIsSignedIn(true)} />
-  ) : (
-    <MainNavigation />
-  );
+  useEffect(() => {
+    configureInternationalization().then(() => setIsInitialized(true));
+  }, []);
+
+  if (!isInitialized) return <View />;
+  else if (!isSignedIn) return <StartNavigation onLoginSuccess={() => setIsSignedIn(true)} />;
+  else return <MainNavigation />;
 };
 
 export default App;
