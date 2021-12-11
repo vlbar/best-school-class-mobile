5fc6621046e07efb851ef9ac245aa986e552c4a9
Pylenkov Roman <solodov1994roma@gmail.com>
2021-12-11 13:33:37 +0300

    updated retry logic

--- App.js
+++ App.js
@@ -2,26 +2,26 @@ import React, { useEffect, useState } from 'react';
 import { View } from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
 
-import StartNavigation from './src/components/navigation/StartNavigation';
+import configureAxios from './src/config/axios-config';
 import MainNavigation from './src/components/navigation/MainNavigation';
-import configureAxios from './config/axios-config';
-import axios from 'axios';
+import StartNavigation from './src/components/navigation/StartNavigation';
 import { configureInternationalization } from './src/utils/Internationalization';
 
-configureAxios(axios);
-
 const App = () => {
-
   const [isSignedIn, setIsSignedIn] = useState(false);
   const [isInitialized, setIsInitialized] = useState(false);
 
   useEffect(() => {
-    configureInternationalization().then(() => setIsInitialized(true));
+    Promise.all([
+      configureAxios(() => setIsSignedIn(false)),
+      configureInternationalization(),
+    ]).then(() => setIsInitialized(true));
     SplashScreen.hide();
   }, []);
 
   if (!isInitialized) return <View />;
-  else if (!isSignedIn) return <StartNavigation onLoginSuccess={() => setIsSignedIn(true)} />;
+  else if (!isSignedIn)
+    return <StartNavigation onLoginSuccess={() => setIsSignedIn(true)} />;
   else return <MainNavigation />;
 };
 
