aff28c8e11d93077f49cf2b3cb561166941072a1
Vladislav Baranov <62684874+vlbar@users.noreply.github.com>
2021-12-12 11:33:30 +0300

    Updated workspace screen

--- src/screens/workspace/Workspace.jsx
+++ src/screens/workspace/Workspace.jsx
@@ -1,24 +1,65 @@
 import React from 'react';
-import { Button } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import moment from 'moment';
+import Icon from 'react-native-vector-icons/Ionicons';
 
+import Color from '../../constants';
 import Container from '../../components/common/Container';
 import Header from '../../components/navigation/Header';
-import IconButton from '../../components/common/IconButton';
 import Text from '../../components/common/Text';
-import { navigatorNames } from '../../navigation/NavigationConstants';
 import { translate } from '../../utils/Internationalization';
 
 export const WORKSPACE_SCREEN = 'workspace';
-function Workspace({ navigation }) {
+function Workspace() {
+  const nearestLessonTime = moment.duration(3, 'day').humanize();
+
   return (
     <>
-      <Header title={translate('workspace.title')} headerRight={<IconButton name="add-outline" />} />
+      <Header title={translate('workspace.title')} />
       <Container>
-        <Text>Workspace...</Text>
-        <Button title="Courses" onPress={() => navigation.navigate(navigatorNames.course)} />
+        <View style={[styles.plannedLessonAlert, styles.notStarted]}>
+          <Text weight='bold' style={[styles.alertText]}>{translate('workspace.planned-lesson.not-started')}</Text>
+          <Text style={[styles.alertText]}>{translate('workspace.planned-lesson.nearest-from')}:</Text>
+          <Text style={[styles.alertText]}>{nearestLessonTime}</Text>
+        </View>
+        <View style={styles.timetableToday}>
+          <Icon name='calendar-outline' size={28} />
+          <Text style={styles.timetableTodayTitle}>{translate('workspace.timetable-today.title')}</Text>
+        </View>
+        <Text style={styles.timetableTodayNoLessons}>{translate('workspace.timetable-today.no-lessons')}</Text>
       </Container>
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  plannedLessonAlert: {
+    paddingVertical: 40,
+    paddingHorizontal: 20,
+    borderRadius: 20,
+  },
+  notStarted: {
+    backgroundColor: Color.lightPrimary,
+  },
+  alertText: {
+    textAlign: 'center',
+    color: Color.white,
+    fontSize: 16,
+  },
+  timetableToday: {
+    marginTop: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  timetableTodayTitle: {
+    marginLeft: 10,
+  },
+  timetableTodayNoLessons: {
+    marginTop: 10,
+    textAlign: 'center',
+    color: Color.silver,
+    fontSize: 14,
+  }
+});
 
 export default Workspace;
