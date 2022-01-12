7a653b36e56b9a435b8b94cbc5128ff3b1f16abb
Vladislav Baranov <62684874+vlbar@users.noreply.github.com>
2022-01-12 08:42:36 +0300

    Updated flex and touchable opacity in buttons

--- src/components/common/IconButton.jsx
+++ src/components/common/IconButton.jsx
@@ -4,7 +4,7 @@ import Icon from 'react-native-vector-icons/Ionicons';
 
 function IconButton({ name, size = 28, color, onPress, style, disabled = false }) {
   return (
-    <TouchableOpacity style={[styles.button, disabled && styles.disabled, style]} disabled={disabled} onPress={onPress}>
+    <TouchableOpacity activeOpacity={0.4} style={[styles.button, disabled && styles.disabled, style]} disabled={disabled} onPress={onPress}>
       <Icon name={name} size={size} color={color} />
     </TouchableOpacity>
   );
