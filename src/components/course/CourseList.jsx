0cb429e5f55df5679e3031ba71da8a89b8a1f9ec
Vladislav Baranov <62684874+vlbar@users.noreply.github.com>
2022-01-19 17:24:11 +0300

    Fixed not full hidding selected course under action menu

--- src/components/course/CourseList.jsx
+++ src/components/course/CourseList.jsx
@@ -336,7 +336,7 @@ const styles = StyleSheet.create({
   },
   listHeader: {
     backgroundColor: Color.white,
-    marginHorizontal: 20,
+    paddingHorizontal: 20,
   },
   searchBar: {
     marginBottom: 0,
